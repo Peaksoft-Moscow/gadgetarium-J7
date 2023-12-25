651ed8f888225d81fb403a5bd45bf1f9bbc7cebe
bakytzamalzorobekova <shamuratovna89@gmail.com>
2023-12-25 13:56:22 +0300

    Sign up with Google

--- src/main/java/com/peaksoft/gadgetariumj7/model/mapper/UserMapper.java
+++ src/main/java/com/peaksoft/gadgetariumj7/model/mapper/UserMapper.java
@@ -1,31 +1,32 @@
 package com.peaksoft.gadgetariumj7.model.mapper;
-
 import com.peaksoft.gadgetariumj7.model.dto.AuthWithGoogleRequest;
 import com.peaksoft.gadgetariumj7.model.dto.AuthWithGoogleResponse;
 import com.peaksoft.gadgetariumj7.model.entities.User;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 @Component
 public class UserMapper {
     public User MapToEntity(AuthWithGoogleRequest request){
         User user = new User();
-        user.setId(request.getId());
-        user.setName(request.getName());
-        user.setUserPic(request.getUserPic());
-        user.setEmail(request.getEmail());
-        user.setLocale(request.getLocal());
-        user.setGender(request.getGender());
+       user.setName(request.getName());
+       user.setEmail(request.getEmail());
+       user.setUserPic(request.getUserPic());
+       user.setGender(request.getGender());
+       user.setLocal(request.getLocal());
+       user.setLastVisit(LocalDateTime.now());
         return user;
     }
 
     public AuthWithGoogleResponse mapToResponse(User user){
         return AuthWithGoogleResponse.builder()
+                .id(user.getId())
                 .name(user.getName())
-                .userPic(user.getUserPic())
                 .email(user.getEmail())
+                .userPic(user.getUserPic())
                 .gender(user.getGender())
-                .local(user.getLocale()).build();
+                .local(user.getLocal())
+            .build();
     }
 }
