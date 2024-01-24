c672435ac88db8b8c0963047894e2a60001b7e8c
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-01-24 12:20:08 +0300

    Create Basket

---
+++ src/main/java/com/peaksoft/gadgetariumj7/controller/BasketController.java
@@ -0,0 +1,29 @@
+package com.peaksoft.gadgetariumj7.controller;
+
+import com.peaksoft.gadgetariumj7.model.dto.BasketResponse;
+import com.peaksoft.gadgetariumj7.service.BasketService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/basket")
+@Slf4j
+public class BasketController {
+    final BasketService basketService;
+@GetMapping("/addToBasket/{id}")
+    public BasketResponse addToBasket(@PathVariable("id")Long productId, Principal principal){
+        return basketService.addToBasket(productId, principal);
+    }
+    @GetMapping("/myBasket")
+    List<BasketResponse> getProductInMyBasket(Principal principal){
+     return basketService.myBasket(principal.getName());
+    }
+
+
+}
